5774218f7c2d45dde2dba24dd9b82f18bd719ec2
Simos Tigkas <stigkas@c1r6p16.hive.fi>
2023-11-01 14:20:38 +0200

    added ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 13:32:48 by stigkas           #+#    #+#             */
+/*   Updated: 2023/11/01 14:19:46 by stigkas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, const char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*copy;
+
+	copy = (char *)malloc((ft_strlen(s1) + 1) * sizeof(char));
+	if (copy == NULL)
+		return (0);
+	return (ft_strcpy(copy, s1));
+}
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*new;
+	unsigned int	i;
+
+	i = 0;
+	if (s[i] == '\0')
+		return (NULL);
+	new = ft_strdup(s);
+	if (!(new))
+		return (NULL);
+	while (s[i])
+	{
+		new[i] = f(i, s[i]);
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
