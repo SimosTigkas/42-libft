67888b72859eb28bdefdef38070ef19b85f22656
Simos Tigkas <stigkas@c1r6p16.hive.fi>
2023-11-02 14:59:43 +0200

    added ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,21 +6,26 @@
 /*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 16:52:17 by stigkas           #+#    #+#             */
-/*   Updated: 2023/10/31 16:52:20 by stigkas          ###   ########.fr       */
+/*   Updated: 2023/11/02 13:39:31 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *str, int c)
 {
-	if (c == '\0')
-		return ((char *)(s + ft_strlen(s)));
-	while (*s)
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
+		if (str[i] == (char)c)
+		{
+			return ((char *)&str[i]);
+		}
+		i++;
 	}
+	if (c == '\0')
+		return ((char *)&str[i]);
 	return (NULL);
 }
