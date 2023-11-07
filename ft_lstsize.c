86ddc44cad932ce2ff66b383a7cac78ef878812a
Simos Tigkas <stigkas@c1r6p18.hive.fi>
2023-11-07 08:49:37 +0200

    added ft_lstsize.c

---
+++ ft_lstsize.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 08:38:19 by stigkas           #+#    #+#             */
+/*   Updated: 2023/11/07 08:48:35 by stigkas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int		size;
+	t_list	*ptr;
+
+	size = 0;
+	if (lst == NULL)
+		return (0);
+	ptr = lst;
+	while (ptr != NULL)
+	{
+		size++;
+		ptr = ptr->next;
+	}
+	return (size);
+}
+
