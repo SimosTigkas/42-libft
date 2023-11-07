8b574e8d7e0ac0362a43237f7f3dd7ae215de1fc
Simos Tigkas <stigkas@c1r6p18.hive.fi>
2023-11-07 08:54:17 +0200

    added changes in ft_lstadd_front and ft_lstsize

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 08:30:20 by stigkas           #+#    #+#             */
-/*   Updated: 2023/11/07 08:36:24 by stigkas          ###   ########.fr       */
+/*   Updated: 2023/11/07 08:52:55 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,3 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 		*lst = new;
 	}
 }
-
