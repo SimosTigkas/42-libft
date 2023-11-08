60a54e55ec0b94e9f7d4d9f94b7aae893780d3d8
Simos Tigkas <stigkas@c1r6p18.hive.fi>
2023-11-08 09:09:27 +0200

    added ft_lstclear.c

---
+++ ft_lstclear.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 08:38:08 by stigkas           #+#    #+#             */
+/*   Updated: 2023/11/08 09:07:45 by stigkas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	if (!del || !(lst))
+		return ;
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		(del)((*lst)->content);
+		*lst = temp;
+		free(*lst);
+	}
+	*lst = NULL;
+}
