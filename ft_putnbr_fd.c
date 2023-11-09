820ff890d895dc49af29f85566785debea776c4d
Simos Tigkas <stigkas@c1r6p18.hive.fi>
2023-11-09 12:31:58 +0200

    added ft_putnbr.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 09:16:23 by stigkas           #+#    #+#             */
-/*   Updated: 2023/11/08 14:03:27 by stigkas          ###   ########.fr       */
+/*   Updated: 2023/11/09 12:26:58 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	buffer[12];
+	char	buffer[13];
 	int		i;
 
-	i = 11;
+	i = 10;
+	buffer[11] = '\0';
 	if (n == 0)
 		ft_putchar_fd('0', fd);
 	else if (n == -2147483648)
