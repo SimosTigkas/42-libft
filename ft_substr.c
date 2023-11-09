65f730a143ba8f7aad1a77779ab3201919f2f0e9
Simos Tigkas <stigkas@c1r6p18.hive.fi>
2023-11-09 15:23:12 +0200

    added ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 17:39:32 by stigkas           #+#    #+#             */
-/*   Updated: 2023/11/06 16:13:33 by stigkas          ###   ########.fr       */
+/*   Updated: 2023/11/09 15:20:10 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,24 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*new_str;
 	size_t	i;
-	size_t	j;
+	size_t	s_len;
 
+	i = 0;
+	s_len = ft_strlen(s);
+	if (!s)
+		return (NULL);
+	if (start >= s_len || len == 0)
+		return (ft_strdup(""));
+	if (len + start > s_len)
+		len = s_len - start;
 	new_str = (char *)malloc(len + 1);
-	if (!s || !(new_str))
-		return (0);
-	i = start;
-	j = 0;
-	while (i < ft_strlen(s) && j < len)
-		new_str[j++] = s[i++];
-	new_str[j] = '\0';
+	if (!new_str)
+		return (NULL);
+	while (i < len)
+	{
+		new_str[i] = s[start + i];
+		i++;
+	}
+	new_str[len] = '\0';
 	return (new_str);
 }
