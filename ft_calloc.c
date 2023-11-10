346b4b62bc76c4df6f183c3924d3590cbe8f0c9c
Simos Tigkas <stigkas@c1r6p18.hive.fi>
2023-11-10 15:39:18 +0200

    added ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 16:39:53 by stigkas           #+#    #+#             */
-/*   Updated: 2023/11/06 13:16:49 by stigkas          ###   ########.fr       */
+/*   Updated: 2023/11/10 15:37:35 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*ptr;
 
 	total_size = count * size;
+	if (total_size < count && total_size < size)
+		return (NULL);
 	ptr = malloc(total_size);
 	if (ptr != NULL)
 		ft_bzero(ptr, total_size);
+	else
+		return (NULL);
 	return (ptr);
 }
