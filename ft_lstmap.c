d761fdb2ceb236205af816be81cc386d47802a74
Simos Tigkas <stigkas@c1r6p18.hive.fi>
2023-11-10 14:14:43 +0200

    added ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 09:40:59 by stigkas           #+#    #+#             */
-/*   Updated: 2023/11/10 13:59:18 by stigkas          ###   ########.fr       */
+/*   Updated: 2023/11/10 14:14:24 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,28 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_list;
+	t_list	*res;
+	t_list	*current;
 	t_list	*new_node;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	new_list = NULL;
-	new_list = ft_lstnew(f(lst->content));
-	if (!(new_list))
+	res = ft_lstnew(f(lst->content));
+	if (!res)
 		return (NULL);
-	new_node = NULL;
+	current = res;
+	lst = lst->next;
 	while (lst)
 	{
 		new_node = ft_lstnew(f(lst->content));
-		if (!(new_node))
+		if (!new_node)
 		{
-			ft_lstclear(&(new_list), del);
+			ft_lstclear(&current, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_list, new_node);
+		current->next = new_node;
+		current = new_node;
 		lst = lst->next;
 	}
-	return (new_list);
+	return (res);
 }
